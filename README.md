453a7002847c9913225f4a88a253cb39e20cfeb5
Anton Yudin <ayudin@epc-instore.com>
2025-11-12 22:58:12 -0500

    add plain template

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ JSF templates
 
 example - an example of how to use the templates.
 		use the "template" property to select which template to use in the example
-		mvn clean package -Dtemplate=default
+```
+mvn clean package -Dtemplate=default
+```
+or
+```
+mvn clean package -Dtemplate=plain
+```
 
---
+++ plain/src/main/java/com/antonyudin/templates/jsf/plain/Menu.java
@@ -0,0 +1,36 @@
+
+package com.antonyudin.templates.jsf.plain;
+
+
+import java.io.Serializable;
+
+import java.util.logging.Logger;
+
+import jakarta.inject.Named;
+
+import jakarta.enterprise.context.SessionScoped;
+
+
+@SessionScoped
+@Named
+public class Menu implements Serializable {
+
+	private final static Logger logger = Logger.getLogger(Menu.class.getName());
+
+
+	private boolean open = true;
+
+	public boolean isOpen() {
+		return open;
+	}
+
+	public void setOpen(final boolean value) {
+		open = value;
+	}
+
+	public void toggleOpen() {
+		setOpen(!isOpen());
+	}
+
+}
+
